1ca3f8990cac4ce4a750503fce70f1d52e02e680
seoyoung1024 <jjssyy0608@gmail.com>
2025-03-13 16:48:05 +0900

    checkprime

---
+++ c/prime.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "libcheckprime.h"
+
+void main() 
+{
+    int n;
+    while (1) {
+    printf("\nInput integer (0:Exit) : ");
+    scanf("%d", &n);
+    if(n == 0) break;
+    if (checkprime(n) ==n)
+            printf("%d is a prime number \n", n);
+        else
+            printf("%d is not prime number \n", n);
+    }
+}
