927d6c7553512a64e9ef3d910730ed5726179c61
seoyoung1024 <jjssyy0608@gmail.com>
2025-03-25 10:16:40 +0900

    python bottle

--- node/hello/hello.js
+++ node/hello/hello.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res){
-    res.send('Hello node js!')
+    res.send('Hello node js!!!!!!!!')
 })
 
 app.listen(8000, function(){
-    console.log("8000 Port : Server Started!")
+    console.log("8000 Port : Server Started!!!!")
 })
