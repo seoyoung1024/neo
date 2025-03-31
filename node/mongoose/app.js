03694609e85e62a6546d93abe55d19374bad7167
seoyoung1024 <jjssyy0608@gmail.com>
2025-03-31 17:42:40 +0900

    node, python

--- node/mongoose/app.js
+++ node/mongoose/app.js
@@ -14,11 +14,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // mongoose configuration
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.1.21:27017/test')
+mongoose.connect('mongodb://192.168.1.34:27017/test')
 
 var main = require('./routes/main');
 app.use('/', main);
 
 app.listen(app.get('port'), function () {
-    console.log("Server is Started~!!" + app.get('port'));
+  console.log("Server is Started~!! Port : " + app.get("port"));
 });
+
+
