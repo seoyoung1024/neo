d086c806736e8613c4e64b3f628165bc9b9804b3
seoyoung1024 <jjssyy0608@gmail.com>
2025-03-31 14:03:19 +0900

    update

---
+++ node/mongodb/app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+ const morgan = require('morgan');
+ const fs = require('fs');
+ const path = require('path');
+ const mongoClient = require('mongodb').MongoClient;
+ 
+ const app = express();
+ app.set('port', process.env.Port || 8000);
+ app.use(morgan('dev'));
+ 
+ var db;
+ var databaseUrl = 'mongodb://192.168.1.21:27017';
+ 
+ app.get('/', (req, res) => {
+     res.send('Web Server Started~!!')
+ })
+ 
+ app.get('/things', (req, res) => {
+     mongoClient.connect(databaseUrl, function (err, client) {
+         if (err != null) {
+             res.json({'count' : 0})
+         } else {
+             db = client.db('test');
+             db.collection('things').find({}).toArray(function (err, result) {
+                 if (err) throw err
+                     console.log('result : ');
+                     console.log(result);
+                     res.json(JSON.stringify(result));
+             })
+         }
+     })
+ })
+ 
+ app.get('/emp', (req, res) => {
+     mongoClient.connect(databaseUrl, function (err, client) {
+         if (err != null) {
+             res.json({'count' : 0})
+         } else {
+             db = client.db('test');
+             db.collection('emp').find({}).toArray(function (err, result) {
+                 if (err) throw err
+                     console.log('result : ');
+                     console.log(result);
+                     res.json(JSON.stringify(result));
+             })
+         }
+     })
+ })
+ 
+ app.get('/primer', (req, res) => {
+     mongoClient.connect(databaseUrl, function (err, client) {
+         if (err != null) {
+             res.json({'count' : 0})
+         } else {
+             db = client.db('test');
+             db.collection('primer').find({}).toArray(function (err, result) {
+                 if (err) throw err
+                     console.log('result : ');
+                     console.log(result);
+                     res.json(JSON.stringify(result));
+             })
+         }
+     })
+ })
+ 
+ app.listen(app.get('port'), function () {
+     console.log("Server is Started~!! " + app.get('port'));
+ });
