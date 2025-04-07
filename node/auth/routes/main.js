1e7aeb78faded6fd4d031c9d54c6de3f7cef8644
seoyoung1024 <jjssyy0608@gmail.com>
2025-04-07 09:34:51 +0900

    update

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -137,6 +137,9 @@ app.get('/select', (req, res) => {
 
 app.get('/selectQuery', (req, res) => {
     const id = req.query.id;
+    if (id == ''){
+        res.write("<script>alert('User-id is empty!');</script>");
+    }else{
     const result = connection.query('select * from user where userid = ?', [id]);   
     console.log(result);
     // res.send(result);
@@ -145,27 +148,75 @@ app.get('/selectQuery', (req, res) => {
     } else {
         template_result(result, res);
     }
+}
 })
+
+
 
 app.post('/insert', (req, res) => { 
     const { id, pw } = req.body;
-    const result = connection.query('insert into user values(?, ?)', [id, pw]);
-    console.log(result);
-    res.redirect('/selectQuery?id=' + req.body.id);
+    if( id == '' || pw == ''){
+        res.write("<script>alert('User-id or Password is empty!');</script>");
+    }else{
+        let result = connection.query('select * from user where userid=?', [id]);
+    if(result.length > 0){
+        res.writeHead(200);
+        var template = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Error</title>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <div>
+                <h3 style="margin-left:30px">Register Failed</h3>
+                <h4 style="margin-left:30px">이미 존재하는 아이디 입니다.</h4>
+                </div>
+            </body>
+            </html>`;
+            res.end(template);
+    }else{
+        result = connection.query('insert into user values(?, ?)', [id, pw]);
+        console.log(result);
+        res.redirect('/selectQuery?id=' + id);
+    }
+}
 })
 
 app.post('/update', (req, res) => {
     const { id, pw } = req.body;
-    const result = connection.query('update user set passwd = ? where userid = ?', [pw, id]);
+    if( id == '' || pw == ''){
+        res.write("<script>alert('User-id or Password is empty!');</script>");
+    }else{
+        let result = connection.query('select * from user where userid=?', [id]);
+        console.log(result);
+        if(result.length == 0){
+            template_nodata(res);
+    }else{
+        const result = connection.query('update user set passwd = ? where userid = ?', [pw, id]);
     console.log(result);
     res.redirect('/selectQuery?id=' + req.body.id);
+    }
+}
+    
 })
 
 app.post('/delete', (req, res) => {
     const id = req.body.id;
-    const result = connection.query('delete from user where userid = ?', [id]);
-    console.log(result);
-    res.redirect('/select');
+    if (id == ''){
+        res.write("<script>alert('User-id or Password is empty!');</script>");
+    }else{
+        let result = connection.query('select * from user where userid=?', [id]);
+        console.log(result);
+        if(result.length == 0){
+            template_nodata(res);
+        }else{
+            const result = connection.query('delete from user where userid = ?', [id]);
+            console.log(result);
+            res.redirect('/select');
+        }
+    }
 })
 
 module.exports = app;
