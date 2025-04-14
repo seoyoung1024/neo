399224c7a71e001d4731df55a14227723f4604d0
seoyoung1024 <jjssyy0608@gmail.com>
2025-04-14 17:29:55 +0900

    movie ranking crowling from cgv

--- python/pythonData/quiz_01.py
+++ python/pythonData/quiz_01.py
@@ -1,31 +1,31 @@
 from bs4 import BeautifulSoup
-
-html = open('/work/neo/html/source/5/ex5-10.html', 'r', encoding='utf-8')
-soup = BeautifulSoup(html, 'html.parser')
-tbody = soup.find('tbody')  # 먼저 tbody를 soup에서 찾아줌
-tds = tbody.find_all('td')  # 그 다음 td를 찾는 거야
-
-
-# print(tds)
-
-from pandas import Series
-import matplotlib.pyplot as plt
+from pandas import DataFrame as df
 import numpy as np
+import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'NanumBarunGothic'
 
-mylist = tds
-print(mylist)
+html = open('../../html/source/5/ex5-10.html', 'r', encoding="utf-8")
+soup = BeautifulSoup(html, 'html.parser')
+
+result = []
+tbody = soup.find("tbody")
+tds = tbody.find_all('td')
+for data in tds:
+    result.append(data.text)
+print(result)
 print('-' * 50)
 
-tds1 = np.reshape(a, (2, 2))
-print(tds1)
+mycolumns = ['이름', '국어', '영어']
 
-# myseries = Series(data=mylist, index=myindex)
-# mylim = [0, myseries.max() + 10]
-# myseries.plot(title='금월 실적', kind='line', ylim=mylim, grid=False, rot=40, use_index=True, color=['b'])
+myframe = df(np.reshape(np.array(result), (4, 3)), columns=mycolumns)
+myframe = myframe.set_index('이름')
+print(myframe)
+print('-' * 50)
 
-# filename = 'quiz_01_scoreGraph.png'
-# plt.savefig(filename, dpi=400, bbox_inches='tight')
-# print(filename + ' saved')
-# plt.show()
+myframe.astype(float).plot(kind='line', title='Score', legend=True)
+
+filename = 'quiz_01_scoreGraph.png'
+plt.savefig(filename, dpi=400, bbox_inches='tight')
+print(filename + ' saved...')
+plt.show()
