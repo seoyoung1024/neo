b67f6beb83bafc8f6ae9b8f7fe60d79cee6de5f5
seoyoung1024 <jjssyy0608@gmail.com>
2025-04-16 18:16:29 +0900

    up

--- python/pythonData/p413_getGeocoding.py
+++ python/pythonData/p413_getGeocoding.py
@@ -3,7 +3,7 @@ import os.path
 import folium, requests  #folium: 지도 시각화를 위한 라이브러리. 위도, 경도 기반으로 지도를 만들 수 있다.
 
 # address = '서울 마포구 신수동 451번지 세양청마루아파트 상가 101호'
-address = '서울특별시 서초구 서초대로74길 33' #검색할 주소 문자열, 이 주소를 갖고 카카오 api에 요청을 보낼 것이다.
+address = '서울특별시 중구 세종대로 110' #검색할 주소 문자열, 이 주소를 갖고 카카오 api에 요청을 보낼 것이다.
 url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + address #카카오에서 제공하는 주소 -> 좌표 변환 api사용 중 query뒤에는 주소가 들어가서 이주소를 카카오에게 넘기고 좌표 정보를 받아옴옴
 
 BASE_DIR = os.path.dirname(os.path.relpath("../../../")) 
@@ -44,7 +44,7 @@ print('주소지:', address)
 print('위도:', latitude)
 print('경도:', longitude)
 
-shopinfo = '비트빌'
+shopinfo = '서울 도서관'
 foli_map = folium.Map(location=[latitude, longitude], zoom_start=17)
 myicon = folium.Icon(color='red', icon='info-sign')
 folium.Marker(location=[latitude, longitude], popup=shopinfo, icon=myicon).add_to(foli_map)
