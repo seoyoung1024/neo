aeb9334ce96c83a470adc6ee185da0641e34c8f0
seoyoung <jjssyy0608@gmail.com>
2025-05-30 14:57:14 +0900

    youtube

--- ai-2/6_youtube/p250_app.py
+++ ai-2/6_youtube/p250_app.py
@@ -1,35 +1,35 @@
+## p250_app.py
+
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
 from youtube_transcript_api.formatters import TextFormatter
 import textwrap
 
 def get_video_id(video_url):
-    video_id = video_url.split('v=')[1][:11]
-
-    return video_id
+    return video_url.split("v=")[1][:11]
 
 video_url = "https://www.youtube.com/watch?v=pSJrML-TTmI"
 video_id = get_video_id(video_url)
 
 try:
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
     try:
         transcript_obj = transcript_list.find_transcript(['ko'])
-    except:
+    except NoTranscriptFound:
         transcript_obj = transcript_list.find_generated_transcript(['ko'])
-    
+
     transcript = transcript_obj.fetch()
-    text_formatter = TextFormatter().format_transcript(transcript)
-    text_info = text_formatterted.replace('\n'," ")
+
+    text_formatter = TextFormatter()
+    text_formatted = text_formatter.format_transcript(transcript)
+    text_info = text_formatted.replace('\n', " ")
 
     shorten_text_info = textwrap.shorten(text_info, width=150, placeholder=' [...이하 생략...]')
     print(shorten_text_info, end='\n')
 
 except TranscriptsDisabled:
-    print("해당 URL의 자막이 비활성화되어 있습니다.")
+    print("이 영상은 자막이 비활성화되어 있습니다.")
 except NoTranscriptFound:
-    print("이 영상에는 한국어 자막(수동자동 생성 포함)이 없습니다.")
+    print("이 영상에는 한국어 자막(수동/자동 생성 포함)이 없습니다.")
 except Exception as e:
-    print("예상치 못한 오류가 발생했습니다.", e)
-
-    
+    print(f"알 수 없는 오류 발생: {e}")
--- ai-2/6_youtube/p251_app.py
+++ ai-2/6_youtube/p251_app.py
@@ -1,39 +1,36 @@
-from email import message
-from urllib3 import response
+## p251_app.py
+
+import openai 
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
 from youtube_transcript_api.formatters import TextFormatter
 import textwrap
 
 def get_video_id(video_url):
-    video_id = video_url.split('v=')[1][:11]
-
-    return video_id
+    return video_url.split("v=")[1][:11]
 
 video_url = "https://www.youtube.com/watch?v=pSJrML-TTmI"
 video_id = get_video_id(video_url)
 
 try:
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
     try:
         transcript_obj = transcript_list.find_transcript(['ko'])
-    except:
+    except NoTranscriptFound:
         transcript_obj = transcript_list.find_generated_transcript(['ko'])
-    
-    transcript = transcript_obj.fetch()
-    text_formatter = TextFormatter().format_transcript(transcript)
-    text_info = text_formatterted.replace('\n'," ")
 
-    shorten_text_info = textwrap.shorten(text_info, width=150, placeholder=' [...이하 생략...]')
-    print(shorten_text_info, end='\n')
+    transcript = transcript_obj.fetch()
+
+    text_formatter = TextFormatter()
+    text_formatted = text_formatter.format_transcript(transcript)
+    text_info = text_formatted.replace('\n', " ")
 
 except TranscriptsDisabled:
-    print("해당 URL의 자막이 비활성화되어 있습니다.")
+    print("이 영상은 자막이 비활성화되어 있습니다.")
 except NoTranscriptFound:
-    print("이 영상에는 한국어 자막(수동자동 생성 포함)이 없습니다.")
+    print("이 영상에는 한국어 자막(수동/자동 생성 포함)이 없습니다.")
 except Exception as e:
-    print("예상치 못한 오류가 발생했습니다.", e)
-
+    print(f"알 수 없는 오류 발생: {e}")
 
 def answer_from_given_info(question_info, prompt):
     user_content = f'{prompt} 다음 내용을 바탕으로 질문으로 답해줘. {question_info}'
@@ -47,6 +44,7 @@ def answer_from_given_info(question_info, prompt):
         messages=message,
         max_tokens=100,
         temperature=0.2,
+        stop = ["."],
     )
 
     return response['choices'][0]['message']['content']
