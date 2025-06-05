83b6866a92d3094b6265f4300c550e8dc0af3b8c
seoyoung <jjssyy0608@gmail.com>
2025-06-05 18:01:19 +0900

    up

--- ai-3/ollama/3_app.py
+++ ai-3/ollama/3_app.py
@@ -6,7 +6,7 @@ img_path = './data/image.png'
 print(os.path.exists(img_path))
 
 response = ollama.chat(
-    model="gemma3",
+    model="llama3.2-vision:latest",
     messages=[
         {"role":"user", "content": "사진 속의 승용차의 차종과 번호를 알려줘아아아", "image": [img_path]}
     ]
